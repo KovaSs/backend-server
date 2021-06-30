0241676082a2142a5211744026974f37c5b92dfd
Denis Isaev / Денис Исаев <sr_relikt@mail.ru>
2021-06-30 03:24:00 +0300

    chore | install express and add settings

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log(`SERVER WORKING`)
+import express from 'express';
+
+const PORT = 5000;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    server: 'working',
+    status: 200,
+    query: req.query
+  })
+});
+
+app.post('/', (req, res) => {
+  res.status(200).json({
+    server: 'working',
+    status: 200,
+    body: req.body
+  })
+});
+
+app.listen(PORT, () => {
+  console.log(`SERVER STARTED ON PORT ${PORT}`);
+});
+
